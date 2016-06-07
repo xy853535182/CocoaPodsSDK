cd38ed47b4fc17ff733e18f73d04cbb66565916a
125520@nd.com <494326923@qq.com>
2016-06-07 14:25:25 +0800

    CocoaPodsSDK.podspec

---
+++ CocoaPodsSDK/CocoaPodsSDK.h
@@ -0,0 +1,15 @@
+//
+//  CocoaPodsSDK.h
+//  CocoaPodsSDK
+//
+//  Created by nd on 16/6/7.
+//  Copyright © 2016年 nd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaPodsSDK : NSObject
+
++ (void)cocoaPodsLog;
+
+@end
---
+++ CocoaPodsSDK/CocoaPodsSDK.m
@@ -0,0 +1,18 @@
+//
+//  CocoaPodsSDK.m
+//  CocoaPodsSDK
+//
+//  Created by nd on 16/6/7.
+//  Copyright © 2016年 nd. All rights reserved.
+//
+
+#import "CocoaPodsSDK.h"
+
+@implementation CocoaPodsSDK
+
++ (void)cocoaPodsLog
+{
+    NSLog(@"cocoaPodsLog  hello work");
+}
+
+@end
